a6f4ebf79066300b441fa38d494de776e21ba2f2
IQuackerzMc <34391112+IQuackerzMc@users.noreply.github.com>
2019-09-03 19:40:35 +1000

    Update

--- src/throabletnt/throabletnt.php
+++ src/throabletnt/throabletnt.php
@@ -1,47 +1,41 @@
+
 <?php
-
-namespace throabletnt;
-
-use pocketmine\plugin\PluginBase;
-use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\ListTag;
-use pocketmine\nbt\tag\ByteTag;
-use pocketmine\nbt\tag\DoubleTag;
-use pocketmine\nbt\tag\FloatTag;
-use pocketmine\event\player\PlayerInteractEvent;
+declare(strict_types=1);
+namespace TNT;
+use pocketmine\block\Solid;
 use pocketmine\entity\Entity;
+use pocketmine\entity\object\FallingBlock;
+use pocketmine\event\entity\EntityExplodeEvent;
+use pocketmine\entity\object\PrimedTNT;
 use pocketmine\event\Listener;
-
-class throabletnt extends PluginBase implements Listener {
-    
-    public function onEnable(){
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\plugin\PluginBase;
+class Main extends PluginBase implements Listener{
+    public function onEnable(): void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-    
-     public function onInteract(PlayerInteractEvent $e){
-         $player = $e->getPlayer();
-         if($e->getItem()->getId() == 46){
-             $nbt = new CompoundTag("", [
-                            "Pos" => new ListTag("Pos", [
-                                new DoubleTag("", $player->x),
-                                new DoubleTag("", $player->y + $player->getEyeHeight()),
-                                new DoubleTag("", $player->z)
-                            ]),
-                            "Motion" => new ListTag("Motion", [
-                                new DoubleTag("", -sin($player->yaw / 180 * M_PI) * cos($player->pitch / 180 * M_PI)),
-                                new DoubleTag("", -sin($player->pitch / 180 * M_PI)),
-                                new DoubleTag("", cos($player->yaw / 180 * M_PI) * cos($player->pitch / 180 * M_PI))
-                            ]),
-                            "Rotation" => new ListTag("Rotation", [
-                                new FloatTag("", $player->yaw),
-                                new FloatTag("", $player->pitch)
-                            ]),
-                            ]);
-                        $f = 2;
-                        $tnt = Entity::createEntity("PrimedTNT", $player->chunk, $nbt, true);
-                        $tnt->setMotion($tnt->getMotion()->multiply($f));   
-                        $tnt->spawnTo($player);
-                        $player->sendMessage("Hi mom");
-       }
+	public function onInteract(PlayerInteractEvent $event): void{
+	    $player = $event->getPlayer();
+	    if($player->getInventory()->getItemInHand()->getId() === Item::TNT){
+	        /* @var PrimedTNT $entity */
+	        $entity = Entity::createEntity("PrimedTNT", $player->getLevel(), Entity::createBaseNBT($player));
+	        $entity->setMotion($player->getDirectionVector()->normalize()->multiply(2));
+	        $entity->spawnToAll();
+        }
+    }
+    public function onExplode(EntityExplodeEvent $event): void{
+        $event->setCancelled();
+        foreach($event->getBlockList() as $block){
+            if($block instanceof Solid){
+                $nbt = Entity::createBaseNBT($block);
+                $nbt->setInt("TileID", $block->getId());
+                $nbt->setByte("Data", $block->getDamage());
+                $entity = new FallingBlock($event->getEntity()->getLevel(), $nbt);
+                $entity->setMotion(new Vector3(0, 1, 0));
+                $entity->spawnToAll();
+            }
+        }
     }
 }
